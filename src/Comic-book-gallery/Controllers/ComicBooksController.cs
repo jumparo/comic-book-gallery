d5abbe513efb70617eaf3102c274ff8adafd04b9
Borislav Yordanov <bobi-npmg@hotmail.com>
2016-11-16 18:45:03 +0200

    Updated Comic Book Detail view to be strongly typed

--- src/Comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/Comic-book-gallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Comic_book_gallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,22 @@ namespace Comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
-
-            ViewBag.SeriesTitle = "The Amazing Me";
-            ViewBag.IssueNumber= 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-man survives... <strong>will Me?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Me",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-man survives... <strong>will Me?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name="Dan Slott",Role="Script"},
+                    new Artist() { Name="Pesho Ivanov",Role="Pensils"},
+                    new Artist() { Name="Ivan Peshov",Role="Inks"},
+                    new Artist() { Name="Koko Rabone",Role="Colors"},
+                    new Artist() { Name="Koko shisharkata",Role="Letters"}
+                }
             };
-            return View();
+
+            return View(comicBook);
 
         }
     }
