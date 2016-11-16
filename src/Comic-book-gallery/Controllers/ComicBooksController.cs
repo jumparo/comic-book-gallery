c3e2b7fd5bf860925e6d7a532302d97d5d884b17
Borislav Yordanov <bobi-npmg@hotmail.com>
2016-11-16 13:43:23 +0200

    Updated Comic Book detail view

--- src/Comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/Comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,6 +10,18 @@ namespace Comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "The Amazing Me";
+            ViewBag.IssueNumber= 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-man survives... <strong>will Me?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
             return View();
 
         }
