3d950b500743ca6cfdfca351dd1b36efd3b17625
Borislav Yordanov <bobi-npmg@hotmail.com>
2016-11-16 12:32:31 +0200

    Added Comic Book Detail view

--- src/Comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/Comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace Comic_book_gallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek ==DayOfWeek.Wednesday)
-            {
-                return new RedirectResult("/");
-            }
-            return Content("Hello from the Controller");
-           //return "Hello from the Controller";
+            return View();
 
         }
     }
