f4a8e47a7a5dcd64acea1780c7b64dcae02cdc0b
Borislav Yordanov <bobi-npmg@hotmail.com>
2016-11-17 19:01:37 +0200

    Updated images and bugs fixed

--- src/Comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/Comic-book-gallery/Controllers/ComicBooksController.cs
@@ -1,20 +1,39 @@
-﻿using Comic_book_gallery.Models;
+﻿using ComicBookGallery.Data;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Comic_book_gallery.Controllers
+namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController:Controller
+    public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
         {
+            _comicBookRepository = new ComicBookRepository();
+        }
 
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
 
-            return View();
+            return View(comicBooks);
+        }
 
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+
+            return View(comicBook);
         }
     }
 }
